1f72db042468d7d99b708b27415a8ab7ee1b42a1
Valentin Lica <licavalentinh@gmail.com>
2023-05-30 22:57:11 +0200

    feat: removed srollbar from textarea, changed max-h of it

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -16,7 +16,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <div className="relative w-full flex-1">
         <textarea
           className={cn(
-            "flex min-h-[10rem] max-h-52 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 transition",
+            "flex min-h-[10rem] max-h-72 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 transition",
             className,
             error && "border-red-600 focus-visible:ring-red-600"
           )}
