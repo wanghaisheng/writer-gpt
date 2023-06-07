b66b9204cc8675b87b2ea0358c6fc8cb3f26cca7
Valentin Lica <licavalentinh@gmail.com>
2023-06-07 08:20:57 +0000

    feat: working on stream output

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,35 @@
+import { ChatCompletionRequestMessage } from "openai";
+
+import { Models } from "@interface/openai";
+
+import { OpenAIStream } from "@lib/gpt-stream";
+
+export type RequestData = {
+  messages?: ChatCompletionRequestMessage[];
+  apiKey?: string;
+  model?: Models;
+};
+
+export const config = {
+  runtime: "edge"
+};
+
+export default async function handler(request: Request) {
+  const { apiKey, messages, model } = (await request.json()) as RequestData;
+
+  if (!apiKey?.trim())
+    return new Response("Please provide api key!", { status: 400 });
+  if (!messages?.length)
+    return new Response("Please provide api key!", { status: 400 });
+  if (!model?.trim())
+    return new Response("Please provide api key!", { status: 400 });
+
+  const stream = await OpenAIStream({
+    apiKey,
+    messages,
+    model,
+    stream: true
+  });
+
+  return new Response(stream);
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { TestForm } from "./form";
+
+type Props = {};
+
+const TestPage = (props: Props) => <TestForm />;
+
+export default TestPage;
